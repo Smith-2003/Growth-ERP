45005613faa8968b4baa631532f81b100068f814
kingsk0 <71212181+kingsk0@users.noreply.github.com>
2024-01-27 16:19:04 +0530

    added little functionality to sidelist items

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,3 +1,49 @@
-document.getElementById("sidelist-btn").addEventListener("click",function (){
+function togglesidelist(){
     document.getElementById("sidelist").classList.toggle("isHidden");
-});
+}
+
+pgname =document.getElementById("page-name");
+function showdashboard(){
+    pgname.textContent = "DASHBOARD";
+    togglesidelist();
+}
+function showcrm(){
+    pgname.textContent = "CRM";
+    togglesidelist();
+}
+function showcontacts(){
+    pgname.textContent = "CONTACTS";
+    togglesidelist();
+}
+function showinventory(){
+    pgname.textContent = "INVENTORY";
+    togglesidelist();
+}
+function showsales(){
+    pgname.textContent = "SALES";
+    togglesidelist();
+}
+function showpurchase(){
+    pgname.textContent = "PURCHASE";
+    togglesidelist();
+}
+function showproduction(){
+    pgname.textContent = "PRODUCTION";
+    togglesidelist();
+}
+function showlogistics(){
+    pgname.textContent = "LOGISTICS";
+    togglesidelist();
+}
+function showaccounts(){
+    pgname.textContent = "ACCOUNTS";
+    togglesidelist();
+}
+function showcampaign(){
+    pgname.textContent = "CAMPAIGN";
+    togglesidelist();
+}
+function showreports(){
+    pgname.textContent = "REPORTS";
+    togglesidelist();
+}
